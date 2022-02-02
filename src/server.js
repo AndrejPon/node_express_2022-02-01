074a2f4ce06ce8ce11aeb15b46b78b2b911ebf6a
Andrejus Ponomariovas <andrejus.pon@gmail.com>
2022-02-02 09:17:38 +0200

    Index create

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const func = require('./functions')
-const cors = require('cors');
+const express = require("express");
+const func = require("./functions");
+const cors = require("cors");
 const app = express();
 
-const PORT = 3000
+const PORT = 3000;
 
 app.use(cors());
 
@@ -16,26 +16,25 @@ app.use(cors());
 // response - atsakymo atgal objektas
 // (request, response) => {ivyks kodas kai ateisime pagal adresa ir metoda}
 
-app.get('/', (request, response) => {
-    response.send('hello back end world');
+app.get("/", (request, response) => {
+  response.send("hello back end world");
 });
-app.get('/api/camel', (request, response) =>{
-    console.log('we got get request to /api/camel');
+app.get("/api/camel", (request, response) => {
+  console.log("we got get request to /api/camel");
 
-    const rez = func.makeCamelCase('Camel goes in a desert');
-    console.log(rez);
-    response.json({message: rez});
-});
-
-app.get('/api/posts', (request, response) =>{
-    const post1 = {
-        title: 'My first post',
-        body: ' Story of my first post',
-    };
-    response.json(post1);
+  const rez = func.makeCamelCase("Camel goes in a desert");
+  console.log(rez);
+  response.json({ message: rez });
 });
 
+app.get("/api/posts", (request, response) => {
+  const post1 = {
+    title: "My first post",
+    body: " Story of my first post",
+  };
+  response.json(post1);
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
